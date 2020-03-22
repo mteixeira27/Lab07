4b8982008597f0b3d7e6323d4a0551a9384be999
mteixeira27 <62511511+mteixeira27@users.noreply.github.com>
2020-03-22 10:57:51 -0700

    Add files via upload

---
+++ lab07.py
@@ -0,0 +1,11 @@
+userName = input("Hello! Please enter your facebook username!")
+userPassword = input("Hello! Please enter your facebook password!")
+
+if(userName.lower() == "monique" and userPassword == "Springtime1!"):
+    print("Welcome to facebook!")
+
+else:
+    print("Wrong credentials, try again!")
+    
+
+
